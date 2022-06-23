a816d7a61aa994a03d80f7d9ccf77e5661d8b2d6
potatosim <devildriver2310@gmail.com>
2022-06-23 16:11:00 +0300

    refactor: optimize code

--- src/index.js
+++ src/index.js
@@ -30,30 +30,22 @@ module.exports = function toReadable(number) {
         90: 'ninety',
     };
 
-    if (number in num) {
-        return num[number];
-    }
-
-    if (number < 100) {
-        const firstNum = Math.floor(number / 10) * 10;
-        const secondNum = number - firstNum;
+    const lessThanHundred = (digits) => { 
+        if (digits in num) {
+            return num[digits];
+        }
+        const firstNum = Math.floor(digits / 10) * 10;
+        const secondNum = digits - firstNum;
         return `${num[firstNum]} ${num[secondNum]}`;
     }
 
-    if (number < 1000) {
+    if (number >= 100) {
         const hundreds = Math.floor(number / 100);
         if (number % 100 === 0) {
             return `${num[hundreds]} hundred`;
         }
         const digits = number - hundreds * 100;
-        if (digits in num) {
-            return `${num[hundreds]} hundred ${num[digits]}`
-        }
-        const firstNum = Math.floor(digits / 10) * 10;
-        const secondNum = digits - firstNum;
-        if (secondNum !== 0) {
-            return `${num[hundreds]} hundred ${num[firstNum]} ${num[secondNum]}`
-        }
-        return `${num[hundreds]} hundred ${num[firstNum]}`;
-    }   
+        return `${num[hundreds]} hundred ${lessThanHundred(digits)}`
+    }
+    return lessThanHundred(number);
 }
